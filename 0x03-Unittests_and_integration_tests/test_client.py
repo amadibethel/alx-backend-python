187fcbc3cafbd0217e3b40985f23d955cc659466
Bethel Amadi <amadibethel@icloud.com>
2025-09-18 14:46:29 +0200

    Add Parameterize and patch as decorators file to Project

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Unit tests for the client.GithubOrgClient class.
 """
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -12,12 +12,12 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """Test cases for GithubOrgClient class."""
 
+    @patch("client.get_json")
     @parameterized.expand([
         ("google",),
         ("abc",)
     ])
-    @patch("client.get_json")
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, mock_get_json, org_name):
         """Test that GithubOrgClient.org returns the expected value."""
         test_payload = {"key": "value"}
         mock_get_json.return_value = test_payload
