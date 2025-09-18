dc82de67841f26691b89577ed0e0c73ed77ba358
Bethel Amadi <amadibethel@icloud.com>
2025-09-18 23:04:32 +0200

    Add Build api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,6 +1,6 @@
 # messaging_app/chats/views.py
 
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters   # added filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
@@ -14,12 +14,10 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     queryset = Conversation.objects.all().prefetch_related("participants", "messages")
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]  #
+    search_fields = ["participants__first_name", "participants__last_name", "participants__email"]  #
 
     def create(self, request, *args, **kwargs):
-        """
-        Create a new conversation with participants.
-        Expects a list of participant user_ids.
-        """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
@@ -35,10 +33,6 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["post"])
     def add_message(self, request, pk=None):
-        """
-        Send a new message to an existing conversation.
-        Expects: { "sender": <user_id>, "message_body": "..." }
-        """
         conversation = get_object_or_404(Conversation, pk=pk)
         serializer = MessageSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -47,9 +41,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
             sender_id=request.data.get("sender"),
             message_body=request.data.get("message_body"),
         )
-        return Response(
-            MessageSerializer(message).data, status=status.HTTP_201_CREATED
-        )
+        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -57,3 +49,5 @@ class MessageViewSet(viewsets.ModelViewSet):
 
     queryset = Message.objects.all().select_related("sender", "conversation")
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]  #
+    search_fields = ["message_body", "sender__email"]  #
