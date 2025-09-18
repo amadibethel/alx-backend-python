e0b853cf48a0c62ff2eb5d63c2add5babb3e0639
Bethel Amadi <amadibethel@icloud.com>
2025-09-19 00:18:16 +0200

    Add Set up url routing file to Project

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,15 @@
 # messaging_app/chats/urls.py
 
 from django.urls import path, include
-from rest_framework import routers   # explicit import
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-# Use routers.DefaultRouter()
+# Create a router and register viewsets
 router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
+# Include router urls in urlpatterns
 urlpatterns = [
     path("", include(router.urls)),
 ]
